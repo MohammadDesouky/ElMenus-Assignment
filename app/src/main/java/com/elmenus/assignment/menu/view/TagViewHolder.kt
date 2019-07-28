baad51268ee9e49dfff343a0d669a8a0662499dd
Mohammad Desouky <MohamamdDesouky@gmail.com>
2019-07-28 02:18:36 +0200

    tag enhancements

--- app/src/main/java/com/elmenus/assignment/menu/view/TagViewHolder.kt
+++ app/src/main/java/com/elmenus/assignment/menu/view/TagViewHolder.kt
@@ -1,6 +1,7 @@
 package com.elmenus.assignment.menu.view
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
@@ -8,7 +9,7 @@ import com.elmenus.assignment.R
 import com.elmenus.assignment.menu.model.Tag
 import kotlinx.android.synthetic.main.tag_item.view.*
 
-class TagViewHolder(parent: ViewGroup, private var selected: Boolean = false) : RecyclerView.ViewHolder(
+class TagViewHolder(parent: ViewGroup, private var isSelected: Boolean = false) : RecyclerView.ViewHolder(
     LayoutInflater.from(parent.context).inflate(R.layout.tag_item, parent, false)
 ) {
     fun bindTagToView(tag: Tag?, onClick: (tag: Tag?) -> Unit) {
@@ -17,7 +18,8 @@ class TagViewHolder(parent: ViewGroup, private var selected: Boolean = false) : 
             .load(tag?.photoURL)
             .into(itemView.tagPhotoImageView)
         itemView.setOnClickListener {
-            selected = true
+            isSelected = true
+            itemView.selectedBackground.visibility = View.VISIBLE
             onClick(tag)
         }
     }
