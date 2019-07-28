66dac74dd2e2a7b610e8090ec15980073343950a
Mohammad Desouky <MohamamdDesouky@gmail.com>
2019-07-28 02:20:38 +0200

    creating the details activity

---
+++ app/src/main/java/com/elmenus/assignment/itemDetails/ItemDetailsActivity.kt
@@ -0,0 +1,13 @@
+package com.elmenus.assignment.itemDetails
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.elmenus.assignment.R
+
+class ItemDetailsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_item_details)
+    }
+}
